d7f3322426de0fa1905a27b13c25a2c0419f2fd8
alfonso <alfonsolasaguirre@gmail.com>
2023-08-02 01:03:03 +0200

    home(feat): screen title

---
+++ app/features/home/components/homeHeader/index.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+
+import AppText from '@app/features/commons/core/text';
+import { FontSize } from '@app/theme/metric';
+
+const HomeHeader = () => {
+  return (
+    <AppText fontSize={FontSize.xxl} style={{ textAlign: 'center' }}>
+      OpositaTest
+    </AppText>
+  );
+};
+
+export default HomeHeader;
--- app/features/home/index.tsx
+++ app/features/home/index.tsx
@@ -4,10 +4,15 @@ import AppLayout from '@app/features/commons/layout';
 import FavoritesBooksSection from '@app/features/home/components/favoritesBooksSection';
 import BooksListSection from '@app/features/home/components/booksListSection';
 import RecentsBooksSection from '@app/features/home/components/recentsBooksSection';
+import { Spacing } from '@app/theme/metric';
+import Spacer from '../commons/spacer';
+import HomeHeader from './components/homeHeader';
 
 const Home = () => {
   return (
     <AppLayout>
+      <HomeHeader />
+      <Spacer size={Spacing.xl} />
       <FavoritesBooksSection />
       <BooksListSection />
       <RecentsBooksSection />
