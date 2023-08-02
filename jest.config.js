a268981fa233c00fa1b8e20142828dc340e830d5
alfonso <alfonsolasaguirre@gmail.com>
2023-08-02 16:47:14 +0200

    testing(feat): set up jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    './setupTests.js',
+  ],
+  // https://github.com/oblador/react-native-vector-icons/issues/1324#issuecomment-823185061
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-safe-area-context)/)',
+  ],
+  setupFiles: [
+    './app/services/__mocks__/bookService.ts',
+    './app/navigation/__mocks__/navigation.ts',
+    './jest-setup.js',
+  ],
 };
