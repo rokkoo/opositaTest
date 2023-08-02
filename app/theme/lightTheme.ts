acca8d891a15cc17c5d108d4f97d6e6372a7fb30
alfonso <alfonsolasaguirre@gmail.com>
2023-08-02 18:45:01 +0200

    theme(feat): update color

--- app/theme/lightTheme.ts
+++ app/theme/lightTheme.ts
@@ -3,7 +3,7 @@ import { AppTheme } from './types';
 const LightTheme: Readonly<AppTheme> = {
   background: '#F7F7F7',
   primary: '#F7F7F7',
-  secondary: '#CBCBCB',
+  secondary: '#FF7C00',
   text: {
     primary: 'black',
     secondary: '#FF7C00',
