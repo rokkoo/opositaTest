fc213a2fcd8d60b8ca25e0f0a5a7dbb56a6cc817
alfonso <alfonsolasaguirre@gmail.com>
2023-08-02 18:44:24 +0200

    core(feat): create loading component

---
+++ app/features/commons/loading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+const Loading = () => {
+  return (
+    <View>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+export default Loading;
