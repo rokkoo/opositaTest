97349f6a6d1b046f8587c70481034ab8198ffe0c
alfonso <alfonsolasaguirre@gmail.com>
2023-08-03 13:12:59 +0200

    testing(feat): rencents visited book hook

---
+++ app/stores/recentsVisitedBooks/__tests__/hooks/useRecentsVisitedBooks.test.ts
@@ -0,0 +1,41 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { RecentsVisitedBooksProvider } from '../..';
+import { useRecentsVisitedBooks } from '../../hooks/useRecentsVisitedBooks';
+import { BooksDTO } from '@app/services/dtos/booksDTO';
+import { mockBooksData } from '@app/services/__mocks__/bookService';
+
+describe('useRecentsVisitedBooks', () => {
+  test('it should add books to the context', () => {
+    const { result } = renderHook(() => useRecentsVisitedBooks(), {
+      wrapper: RecentsVisitedBooksProvider,
+    });
+
+    // the inital value
+    expect(result.current.books).toHaveLength(0);
+    const book = BooksDTO.getArray(mockBooksData)[0];
+
+    act(() => {
+      result.current.addBook(book);
+    });
+
+    expect(result.current.books[0]).toEqual(book);
+    expect(result.current.books).toHaveLength(1);
+  });
+
+  test('should add books to the context and update hasRecentsVisitedBooks', () => {
+    const { result } = renderHook(() => useRecentsVisitedBooks(), {
+      wrapper: RecentsVisitedBooksProvider,
+    });
+
+    // the inital value
+    expect(result.current.hasRecentsVisitedBooks).toBe(false);
+
+    act(() => {
+      const book = BooksDTO.getArray(mockBooksData)[0];
+      result.current.addBook(book);
+    });
+
+    expect(result.current.books).toHaveLength(1);
+    expect(result.current.hasRecentsVisitedBooks).toBe(true);
+  });
+});
