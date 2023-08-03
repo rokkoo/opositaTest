d87e05debe32f6bcfc89b9542bd45446c5035797
alfonso <alfonsolasaguirre@gmail.com>
2023-08-03 20:22:22 +0200

    home(feat): toggle theme schema

--- app/features/home/components/homeHeader/index.tsx
+++ app/features/home/components/homeHeader/index.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import AppText from '@app/features/commons/core/text';
 import { FontSize } from '@app/theme/metric';
+import { useAppTheme } from '@app/theme/hooks/useTheme';
 
 const HomeHeader = () => {
+  const { toggleTheme } = useAppTheme();
+
   return (
-    <AppText
-      fontSize={FontSize.xxl}
-      style={{ textAlign: 'center' }}
-      testID="HomeScreenTitle"
-    >
-      OpositaTest
-    </AppText>
+    <TouchableOpacity onPress={toggleTheme}>
+      <AppText
+        fontSize={FontSize.xxl}
+        style={{ textAlign: 'center' }}
+        testID="HomeScreenTitle"
+      >
+        OpositaTest
+      </AppText>
+    </TouchableOpacity>
   );
 };
 
