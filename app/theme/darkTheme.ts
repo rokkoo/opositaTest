1a7105cd86a95eada6952ed47ac907c3e6d70618
alfonso <alfonsolasaguirre@gmail.com>
2023-08-03 17:26:49 +0200

    theme(feat): add dark new color

--- app/theme/darkTheme.ts
+++ app/theme/darkTheme.ts
@@ -4,6 +4,7 @@ const DarkTheme: Readonly<AppTheme> = {
   background: '#151515',
   primary: '#F7F7F7',
   secondary: '#CBCBCB',
+  tertiary: '#9D9D9D',
   text: {
     primary: 'white',
     secondary: 'gray',
